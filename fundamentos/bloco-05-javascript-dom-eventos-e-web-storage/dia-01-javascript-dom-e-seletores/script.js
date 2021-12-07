b7dedf60bc6ba1be1dc4c88ec591d7bb37ab4e39
Thiago Mota <th.araujomota@gmail.com>
2021-12-07 12:11:21 -0300

    Exemplo Course aula 1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -0,0 +1,13 @@
+document.getElementById('paragraph').style.color = 'green';
+
+document.getElementById('page-title').innerText = 'Viva, a vida é uma festa';
+
+document.getElementById('subtitle').innerText = 'Outros filmes interessantes:';
+
+//a função abaixo não está funcionando corretamente pois o querySelector puxa apenas um único elemento (e o primeiro que for selecionado), não sendo possível puxar o h2 e o h2 juntos.
+document.querySelector('h2', 'h3').classList.add('paragrafo');
+
+document.getElementsByClassName('paragrafo')[0].style.color = 'blue';
+
+document.getElementsByTagName('h4')[0].style.color = 'red'
+document.getElementsByTagName('h4')[1].style.color = 'yellow'
